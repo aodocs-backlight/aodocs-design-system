5cd3058b144a6f6ff69e59f33833acc5e29c1565
aodocs-backlight@gmail.com <93133914+undefined@users.noreply.github.com>
2021-11-30 08:58:31 +0000

    Improve arrow

--- molecule_aodocs-tooltip/src/aodocs-tooltip.ts
+++ molecule_aodocs-tooltip/src/aodocs-tooltip.ts
@@ -2,10 +2,12 @@ import { LitElement, html, css } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import {elevation1} from '../../style_elevation';
 import {paddingsRem} from '../../style_spacing';
+import {zIndexMixin} from '../../style_positioning';
 
 @customElement('aodocs-tooltip')
 export class AodocsTooltip extends LitElement {
   static styles = [
+    zIndexMixin(100),
     paddingsRem,
     elevation1,
     css`
@@ -21,26 +23,40 @@ export class AodocsTooltip extends LitElement {
         text-align: center;
         border-radius: 6px;
         position: absolute;
-        z-index: 1;
-        top: 125%;
+        top: 200%;
         right: 1rem;
         margin-left: -60px;
         opacity: 0;
         transition: opacity 0.3s;
       }
 
-      .aodocs-tooltip .aodocs-tooltip__content::before {
-        content: "";
-        position: absolute;
-        bottom: 100%;
-        left: 80%;
-        transform: rotate(180deg);
-        border-width: 15px;
-        border-style: solid;
-        border-color: white transparent transparent transparent;
+      .aodocs-tooltip:hover .aodocs-tooltip__content {
+        visibility: visible;
+        opacity: 1;
       }
 
-      .aodocs-tooltip:hover .aodocs-tooltip__content {
+      .aodocs-tooltip .aodocs-tooltip__content .arrow {
+        width: 60px;
+        height: 30px;
+        position: absolute;
+        bottom: 100%;
+        right: 0;
+        transform: translateX(-50%);
+        overflow: hidden;
+      }
+      .aodocs-tooltip .aodocs-tooltip__content .arrow:after {
+        content: "";
+        position: absolute;
+        width: 25px;
+        height: 25px;
+        background: var(--mdc-theme-white);;
+        transform: translateX(-50%) translateY(50%) rotate(45deg);
+        bottom: 0;
+        left: 50%;
+        box-shadow: 0 2px 6px 0 rgba(0,0,0,0.2);
+      }
+
+      .aodocs-tooltip:hover .aodocs-tooltip__content .arrow {
         visibility: visible;
         opacity: 1;
       }
@@ -65,7 +81,8 @@ export class AodocsTooltip extends LitElement {
         <div style="position: relative" style="min-width: ${this.minWidth};"> 
           <slot name="main">${this.displayText}</slot>
         </div>
-        <div class="aodocs-tooltip__content elevation-1 pa-1--rem" style="min-width: ${this.minWidthTooltip};">
+        <div class="aodocs-tooltip__content elevation-1 pa-1--rem z-index-100" style="min-width: ${this.minWidthTooltip};">
+          <div class="arrow"></div>
           <slot name="tooltip"></slot>
         </div>
       </div>
